09170d5f1571982b79631383fb9f90619e239201
Maarten Mulders <m.th.mulders@xs4all.nl>
2018-12-10 21:54:44 +0100

    Disable AOT during native image generation
    Not sure what the effect of this is...

--- native/make_native.sh
+++ native/make_native.sh
@@ -2,7 +2,16 @@
 
 [[ -d target ]] || mkdir target
 
+#
+# Setting -Dcom.oracle.graalvm.isaot=false is not in the Simple Language example.
+# By default, it is set to true. But in CircleCI builds, that run on Linux, this setting combined with the fact that
+# Linux has procfs seems to make native-image crash unpredictably.
+#
+
 $JAVA_HOME/bin/native-image \
+    --verbose \
+    --no-server \
+    -Dcom.oracle.graalvm.isaot=false \
     --tool:truffle \
     -H:MaxRuntimeCompileMethods=1200 \
     -H:+ReportUnsupportedElementsAtRuntime \
