851b019d1acb50e55ce387a9c03ea13935f98eff
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-03-06 10:18:05 +0100

    Add an actual assertion
    Even though the test will probably fail when Spring
    cannot build an Application Context.

--- demo-app/src/test/java/it/mulders/brainfuckjvm/demoapp/DemoAppApplicationTests.java
+++ demo-app/src/test/java/it/mulders/brainfuckjvm/demoapp/DemoAppApplicationTests.java
@@ -2,15 +2,23 @@ package it.mulders.brainfuckjvm.demoapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoAppApplicationTests {
+	@Autowired
+	private BrainfuckEngine engine;
 
 	@Test
 	public void contextLoads() {
+		assertThat(engine, is(notNullValue()));
 	}
 
 }
