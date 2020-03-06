e76a4467cc9ec23840a5afd5171e41c72f9a4dc0
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-03-06 10:01:22 +0100

    Remove unused import

--- demo-app/src/main/java/it/mulders/brainfuckjvm/demoapp/RunProgramController.java
+++ demo-app/src/main/java/it/mulders/brainfuckjvm/demoapp/RunProgramController.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
