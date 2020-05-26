cef11f358c62aa753c1b36c7fa809e5810ae5b0a
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-05-26 08:29:43 +0200

    Revert "Brainfuck implementation doesn't have runtime options"
    This reverts commit 0bf35a69a065699394655b88951d4cc82fde33b2.

--- launcher/src/test/java/it/mulders/brainfuckjvm/launcher/LauncherTest.java
+++ launcher/src/test/java/it/mulders/brainfuckjvm/launcher/LauncherTest.java
@@ -1,6 +1,7 @@
 package it.mulders.brainfuckjvm.launcher;
 
 import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
@@ -9,6 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static it.mulders.brainfuckjvm.launcher.Launcher.isOption;
+import static it.mulders.brainfuckjvm.launcher.Launcher.parseOption;
 
 public class LauncherTest implements WithAssertions {
     @Test
@@ -19,6 +21,40 @@ public class LauncherTest implements WithAssertions {
     @Test
     void args_dont_start_with_double_dash() {
         assertThat(isOption("bla.bf")).isFalse();
+    }
+
+    @Test
+    void option_without_value_is_true() {
+        final Map<String, String> options = parseOption("--inspect");
+        final Map.Entry<String, String> option = options.entrySet().iterator().next();
+        assertThat(option.getKey()).isEqualTo("inspect");
+        assertThat(option.getValue()).isEqualTo("true");
+    }
+
+    @Test
+    void option_with_value_keeps_value() {
+        final Map<String, String> options = parseOption("--inspect=localhost:2345");
+        final Map.Entry<String, String> option = options.entrySet().iterator().next();
+        assertThat(option.getKey()).isEqualTo("inspect");
+        assertThat(option.getValue()).isEqualTo("localhost:2345");
+    }
+
+    @Test
+    @Disabled("See question in https://github.com/graalvm/simplelanguage/issues/60")
+    void option_with_subvalue_keeps_value() {
+        final Map<String, String> options = parseOption("--inspect.WaitAttached=false");
+        final Map.Entry<String, String> option = options.entrySet().iterator().next();
+        assertThat(option.getKey()).isEqualTo("WaitAttached");
+        assertThat(option.getValue()).isEqualTo("false");
+    }
+
+    @Test
+    void preprocessArguments_should_move_known_options() {
+        final Map<String, String> options = new HashMap<>();
+        final List<String> args = Collections.singletonList("--inspect.WaitAttached=false");
+        final List<String> remainder = new Launcher().preprocessArguments(args, options);
+        assertThat(remainder).isEmpty();
+        assertThat(options).containsOnly(entry("inspect.WaitAttached", "false"));
     }
 
     @Test
